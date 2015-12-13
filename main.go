4ab020b17ad6ab54da73a7c3dceef0411ba56ed9
Anton Mezhanskiy <hello@imai.kz>
2015-12-13 15:26:41 +0600

    add package models

--- main.go
+++ main.go
@@ -1,40 +1,21 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
 
+	"github.com/AntonMezhanskiy/changelog/models"
 	"github.com/julienschmidt/httprouter"
-)
-
-type Post struct {
-	Title, PublicDate, ViewId, Body string
-}
-
-const (
-	time_layout = "2006-01-02"
-	time_format = "January 02, 2006"
 )
 
 func main() {
 	router := httprouter.New()
 
 	router.ServeFiles("/static/*filepath", http.Dir("static/"))
-	router.GET("/", Index)
-	router.GET("/signin/", SignIn)
-	router.POST("/signin/", SignInPost)
-	router.GET("/entry/:entry/", Entry)
+	router.GET("/", models.Index)
+	router.GET("/signin/", models.SignIn)
+	router.POST("/signin/", models.SignInPost)
+	router.GET("/entry/:entry/", models.Entry)
 
 	log.Fatal(http.ListenAndServe(":8888", router))
-}
-
-func checkErr(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func MarkDown(x string) interface{} {
-	return template.HTML(x)
 }
---
+++ models/utils.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"html/template"
+)
+
+type Post struct {
+	Title, PublicDate, ViewId, Body string
+}
+
+const (
+	time_layout = "2006-01-02"
+	time_format = "January 02, 2006"
+)
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func MarkDown(x string) interface{} {
+	return template.HTML(x)
+}
